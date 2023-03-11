2b94e21f88643782609a5447a11f796b8635397d
LysovMaxim <max_maran@icloud.com>
2023-03-11 13:13:37 +0200

    Добавив код в js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const pictures = createImg(galleryItems);
+
+galleryEl.insertAdjacentHTML("afterbegin", pictures);
+
+function createImg(galleryItems) {
+    return galleryItems.map(({preview, original, description }) => {
+        return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+    }).join("");
+};
+
+var lightbox = new SimpleLightbox('.gallery a',
+    {
+     captionsData: 'alt',
+     captionDelay: 250,
+     scrollZoom: false, });
