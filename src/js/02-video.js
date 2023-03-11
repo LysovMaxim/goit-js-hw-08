87858a2a419d53ca2f1ce5b551a05733ef5e72f7
LysovMaxim <max_maran@icloud.com>
2023-03-11 23:16:52 +0200

    Добавив альтернативний запис поточного часу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-    
+  
 player.on('timeupdate', throttle(function (data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))   
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))  
 }, 1000));
 
+//  альтернативний запис поточного часу
 const correntTime = localStorage.getItem("videoplayer-current-time");
-
 
 const time = JSON.parse(correntTime);
 
-player
-    .setCurrentTime(time.seconds)
-    .then(function (seconds) {
-    
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            
-            break;
-
-        default:
-            
-            break;
-    }
-});
+player.setCurrentTime(time.seconds || 0);
