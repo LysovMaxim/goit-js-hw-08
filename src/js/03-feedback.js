098101077557054f02a4c0378c8c1cb11c47c173
LysovMaxim <max_maran@icloud.com>
2023-03-12 18:13:58 +0200

    Пофыксив 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".feedback-form");
+console.log(formEl);
 
+
+
+formEl.addEventListener("input", (event) => {
+      const {
+    elements: { email, message }
+  } = event.currentTarget;
+    localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }))
+
+    const messages = localStorage.getItem("feedback-form-state")
+    console.log(messages)
+
+    const parseMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(parseMessage)
+   
+});
