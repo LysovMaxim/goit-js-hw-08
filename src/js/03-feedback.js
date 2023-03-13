0f79d2f4ce0b2e9455684f2b72115079eefa0f73
LysovMaxim <max_maran@icloud.com>
2023-03-13 10:13:37 +0200

    Пофыксив 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -17,8 +17,8 @@ saveForm();
 function saveForm() {
   const complectedForm = JSON.parse(localStorage.getItem(CURRENT_VALUE));
   if (complectedForm) {
-    form.email.value = complectedForm.email;
-    form.message.value = complectedForm.message;
+    form.email.value = complectedForm.email || "";
+    form.message.value = complectedForm.message || "";
   }
 }
 
@@ -31,29 +31,4 @@ form.addEventListener('submit', event => {
 });
 
 
-// const throttle = require('lodash.throttle');
-// const STORAGE_KEY = 'feedback-form-state';const formData = {};
-// const form = document.querySelector('.feedback-form');
-// form.addEventListener('submit', onFormSubmit);
-// form.addEventListener('input', throttle(e => { formData[e.target.name] = e.target.value; onInputFormValue(); }, 500));
-// populateFormInput();
-// function onFormSubmit(e) {  e.preventDefault();
-//   console.log(formData);
-//     e.currentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-//     Object.keys(formData).forEach(el => delete formData[el]);
-// }
-// function onInputFormValue() {
-//     const inputValue = JSON.stringify(formData);
-//     localStorage.setItem(STORAGE_KEY, inputValue);
-// }
-// function populateFormInput() {  const storageData = localStorage.getItem(STORAGE_KEY);
-//   if (!storageData) {    return;  }
-//   try {    const savedData = JSON.parse(storageData);
-//         if (savedData) {
-//             Object.entries(savedData).forEach(([name, value]) => {
-//                 const input = form.querySelector(`[name="${name}"]`); input.value = value; formData[name] = value;
-//             });
-//         }
-//     } catch (error) { console.error(error); }
-// }
+
